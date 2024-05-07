714a004c7588b8a3f726867ed3233ec49e5058f9
Billy <1015018684@qq.com>
2024-05-08 01:20:46 +0800

    init

---
+++ src/components/EasyCheckbox.vue
@@ -0,0 +1,23 @@
+<template>
+  <label class="easy-checkbox" :class="{ 'is-checked': value }">
+    <span class="easy-checkbox__input" :class="{ 'is-checked': value }">
+      <span class="easy-checkbox__inner"></span>
+      <input
+        type="checkbox"
+        aria-hidden="false"
+        class="easy-checkbox__original"
+        value=""
+      />
+    </span>
+  </label>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class EasyCheckbox extends Vue {
+  @Prop({ type: Boolean, default: false }) private value!: boolean;
+}
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <EasyCheckbox />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import EasyCheckbox from "@/components/EasyCheckbox.vue";
+
+@Component({ components: { EasyCheckbox } })
+export default class HomeView extends Vue {}
+</script>
